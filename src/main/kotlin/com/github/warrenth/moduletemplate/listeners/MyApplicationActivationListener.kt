a5589d299f08d7915245f0b790045aeeabb9a7b8
GitHub Action <action@github.com>
2024-03-22 01:18:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/warrenth/moduletemplate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/warrenth/moduletemplate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.warrenth.moduletemplate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/warrenth/moduletemplate/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/warrenth/moduletemplate/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.warrenth.moduletemplate.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.warrenth.moduletemplate.MyBundle
+import com.github.warrenth.moduletemplate.services.MyProjectService
 import javax.swing.JButton
 
 
